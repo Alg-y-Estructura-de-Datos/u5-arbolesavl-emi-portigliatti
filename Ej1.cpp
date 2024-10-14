49cc3fb469a05529f68da8ec1273c9c2f7a72aac
EmiPorti <porttmariaemilia@gmail.com>
2024-10-14 08:14:55 -0300

    Arboles AVL

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,85 @@
 #include <iostream>
+#include "Arbol/ArbolBinarioAVL.h"
 using namespace std;
+
+
+void menu(){
+    cout << "1. Insertar un codigo de producto" << endl;
+    cout << "2. Eliminar un codigo de producto" << endl;
+    cout << "3. Buscar un codigo de producto" << endl;
+    cout << "4. Imprimir el arbol" << endl;
+    cout << "5. Salir" << endl;
+    cout << "Ingrese una opcion: ";
+
+}
+
+
 
 int main() {
 
-    cout << "Ejercicio N° 1" << endl;
+    ArbolBinarioAVL<int> productos;
+    int opc, codigo;
 
-    return 0;
+    do
+    {
+        menu();
+        cin >> opc;
+        switch (opc)
+        {
+        case 1:
+            cout << "Ingrese el codigo del producto: ";
+            cin >> codigo;
+            productos.put(codigo);
+            try
+            {
+                productos.put(codigo);
+                cout<<"Producto con codigo "<<codigo<<" insertado correctamente"<<endl;
+            }
+            catch(int e)
+            {
+                if(e==404){
+                    cout<<"Error: el codigo es invalido"<<endl;
+                }
+            }
+            break;
+        case 2:
+            cout << "Ingrese el codigo del producto a eliminar: ";
+            cin >> codigo;
+            try
+            {
+                productos.remove(codigo);
+                cout<<"Producto con codigo "<<codigo<<" eliminado correctamente"<<endl;
+            }
+            catch(int e)
+            {
+                cout<<"Error: el codigo no existe o es invalido"<<endl;
+            }
+            break;
+        case 3:
+            cout << "Ingrese el codigo del producto a buscar: ";
+            cin >> codigo;
+            try
+            {
+                int encontrado=productos.search(codigo);
+                cout<<"Producto con codigo "<<codigo<<" encontrado"<<endl;
+            }
+            catch(int e)
+            {
+                cout<<"Error: el codigo no existe o es invalido"<<endl;
+            }
+            break;
+        case 4:
+            cout<<"Arbol de productos: "<<endl;
+            productos.print();
+            break;
+        case 5:
+            cout << "Saliendo..." << endl;
+            break;
+        default:
+            cout << "Opcion invalida" << endl;
+            break;
+        }
+
+    } while (opc != 5);
+    
 }
--- Ej2.cpp
+++ Ej2.cpp
@@ -1,9 +1,87 @@
 #include <iostream>
+#include "Arbol/ArbolBinarioAVL.h"
 using namespace std;
+
+void menu(){
+    cout<<"1. Agregar producto: inserta un nuevo codigo de producto en el sistema. "<<endl;
+    cout<<"2. Eliminar producto: elimina un codigo de producto del sistema. "<<endl;
+    cout<<"3. Buscar producto: verifica si un codigo de producto existe en el inventario. "<<endl;
+    cout<<"4. Mostrar inventario: imprime el arbol AVL de productos para mostrar su estrcutra balanceada. "<<endl;
+    cout<<"5. Mostrar el numero de balance del arbol. "<<endl;
+    cout<<"6. Salir. "<<endl;
+}
 
 int main() {
 
-    cout << "Ejercicio N° 2" << endl;
+    ArbolBinarioAVL<int> productos;
+    int opc, codigo;
 
-    return 0;
+    do
+    {
+        menu();
+        cin>>opc;
+        switch (opc)
+        {
+        case 1:
+            cout<<"Ingrese el codigo del producto: ";
+            cin>>codigo;
+            try
+            {
+                productos.put(codigo);
+                cout<<"Producto con codigo "<<codigo<<" insertado correctamente"<<endl;
+            }
+            catch(int e)
+            {
+                if(e==200){
+                    cout<<"Error: el codigo del producto ya existe en el inventario."<<endl;
+                }
+            }
+            break;
+        case 2:
+            cout<<"Ingrese el codigo del producto: ";
+            cin>>codigo;
+            try
+            {
+                productos.remove(codigo);
+                cout<<"Producto con codigo "<<codigo<<" eliminado correctamente"<<endl;
+            }
+            catch(int e)
+            {
+                if(e==404){
+                    cout<<"Error: el codigo del producto no existe en el inventario."<<endl;
+                }
+            }
+            break;
+        case 3:
+            cout<<"Ingrese el codigo del producto a buscar: ";
+            cin>>codigo;
+            try
+            {
+                int encontrado=productos.search(codigo);
+                cout<<"Producto con codigo "<<codigo<<" encontrado en el inventario"<<endl;
+            }
+            catch(int e)
+            {
+                if(e==404){
+                    cout<<"Error: el codigo del producto no existe en el inventario."<<endl;
+                }
+            }
+            break;
+        case 4:
+            cout<<"Inventario: "<<endl;
+            productos.print();
+            break;
+        case 5:
+            cout<<"Numero de balance del arbol: "<<productos.getBalance()<<endl;
+            break;
+        case 6:
+            cout<<"Fin del programa"<<endl;
+            break;
+            
+        default:
+            cout<<"Opcion invalida. Intente nuevamente."<<endl;
+            break;
+        }
+    } while (opc!=6);
+    
 }
